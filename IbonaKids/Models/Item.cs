2dc74bb444fc59212c7b72c321c2efa4d113bfe6
cygnn <cpuntanar4@gmail.com>
2026-04-23 19:07:23 +0800

    feat: item model attributes

---
+++ IbonaKids/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IbonaKids.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public required string ItemName { get; set; }
+        [Required]
+        public required string ItemDescription { get; set; }
+        [Required]
+        public required decimal Price { get; set; }
+        [Url]
+        public string? ImageUrl { get; set; }
+
+    }
+}
