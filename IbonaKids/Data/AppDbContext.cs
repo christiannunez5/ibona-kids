227b830dc239d4bdf8b58d33a34b7e80a5e85bfd
christiannunez5 <christiannunez775@gmail.com>
2026-04-23 18:36:36 +0800

    chore: database connection

---
+++ IbonaKids/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IbonaKids.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
--- IbonaKids/Program.cs
+++ IbonaKids/Program.cs
@@ -1,15 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using IbonaKids.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddDbContext<AppDbContext>(opts =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        opts.UseSqlServer(connectionString);
+    });
+}
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
