df815618e6719c6d708cb3962263e05a978d5deb
alaugustusembrace <alaugustuscayacap@gmail.com>
2026-05-01 16:14:26 +0800

    feat: added order model -Cayacap

---
+++ IbonaKids/Migrations/20260501080517_AddOrder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IbonaKids.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ IbonaKids/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IbonaKids.Models
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Paid,
+        Cancelled
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Precision(18, 2)]
+        public decimal TotalAmount { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+        //public ICollection<Item> OrderItems { get; set; } = new List<Item>();
+    }
+}
