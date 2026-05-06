c7be5285ca1421f1115ef3461be7f76713252ad6
Zuka <marcjacoblasam123@gmail.com>
2026-05-06 19:22:08 +0800

    feat: add CartItem

---
+++ IbonaKids/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IbonaKids.ViewModels
+{
+    public class CartItemViewModel
+    {
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
